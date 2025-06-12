63135dd345ccc6e402d9f481a4be7637b8aa952b
Yancarlos <yancarlos_cartagena82171@elpoli.edu.co>
2025-06-12 17:37:40 -0500

    script runChrome and spec.cy.js test

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,17 @@
-describe('My First Test', () => {
-  it('Does not do much!', () => {
-    expect(true).to.equal(true)
+describe('Prueba automática en example.cypress.io', () => {
+  it('Verifica navegación y elementos visibles', () => {
+    cy.visit('https://example.cypress.io')
+
+    // Abre el menú desplegable
+    cy.get('.dropdown-toggle').contains('Commands').click()
+
+    // Ahora el enlace "Querying" está visible y se puede hacer clic
+    cy.contains('Querying').click()
+
+    // Verifica que esté en la página correcta
+    cy.url().should('include', '/commands/querying')
+
+    // Verifica que el botón existe
+    cy.get('#query-btn').should('exist').and('contain', 'Button')
   })
 })
