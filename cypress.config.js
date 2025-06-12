a0f6fea7f4f42727d29184f8936deaddd5cf4f1a
Yancarlos <yancarlos_cartagena82171@elpoli.edu.co>
2025-06-12 18:12:45 -0500

    edit cypress.config.js for reports in mochawesome

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,17 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: 'cypress/reports',
+    overwrite: false,
+    html: true,
+    json: true
+  },
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
-});
+      // Puedes dejarlo vacío si no usas plugins
+    }
+  }
+})
+
